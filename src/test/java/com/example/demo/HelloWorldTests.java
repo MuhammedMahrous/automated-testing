cd18f3a3441353a24d8bdce47d6b24b1f4b6dda5
Muhamed Mahrous <muhamed.mahrous.muhamed.kamel@gmail.com>
2020-03-29 22:47:25 +0200

    inital

---
+++ src/test/java/com/example/demo/HelloWorldTests.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+// TODO: 7. Life Cycle
+public class HelloWorldTests {
+    // TODO: 6. Setup
+    /* BeforeEach vs Before All (and TestInstance)
+     * */
+
+    // TODO: 8. Unit Test
+    /**
+     * - @Test
+     * - @DisplayName class and method
+     * - assertTrue() and assertFalse()
+     * - assertNull() and assertNull()
+     * - assertEquals()
+     * - assertAll()
+     * - assertThrows()
+     * - @Disabled
+     */
+}
