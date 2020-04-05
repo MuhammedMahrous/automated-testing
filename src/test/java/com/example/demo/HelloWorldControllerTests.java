853550c0ed83015c5931776527b3525c0b6f4584
Muhamed Mahrous <muhamed.mahrous.muhamed.kamel@gmail.com>
2020-04-06 01:07:25 +0200

    updates.

--- src/test/java/com/example/demo/HelloWorldControllerTests.java
+++ src/test/java/com/example/demo/HelloWorldControllerTests.java
@@ -6,17 +6,16 @@ import org.springframework.test.web.servlet.MockMvc;
 public class HelloWorldControllerTests {
 
     /**
-     * TODO (2.1): @WebMvcTest
+     * TODO (2.1): @WebMvcTest and and ExtendWith and its auto configuration
      * TODO (2.2): @ComponentScan
      * TODO (2.3): MockMvc
      * TODO (2.4): simple get(), status().isOk()
-     * TODO (2.5):  get(), status().isBadRequest()
+     * TODO (2.5):  get(), status().isBadRequest() and param()
      * TODO (2.6):  get(), status().isOk() and content().contentType() content().json()
-     * TODO (2.7):  get(), status().isOk() and jsonPath(,hamcrest)
-     * TODO (2.8): andReturn().getResponse()
+     * TODO (2.7):  get(), status().isOk() and jsonPath(,Matchers.is())
+     * TODO (2.8): andReturn().getResponse() => handler
      * TODO (2.9): andDo()
      */
-
     private MockMvc mockMvc;
 
     @Test
